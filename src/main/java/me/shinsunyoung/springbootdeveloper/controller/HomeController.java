74b28027555b65267ff3e7f37df7cd713c377efa
YoonKiBum <ykbsuper@naver.com>
2024-05-16 12:31:27 +0900

    project init

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/HomeController.java
@@ -0,0 +1,13 @@
+package me.shinsunyoung.springbootdeveloper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectMain() {
+        return "redirect:/login";
+    }
+}
